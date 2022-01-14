f9a5f8fe71da079ed465abc7d36d5671088db1b2
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-14 20:33:48 +0100

    Add some test in ParticipantDaoTestFile and change config file for test database

--- src/test/java/tsi/ensg/jee/services/EvenementDaoTest.java
+++ src/test/java/tsi/ensg/jee/services/EvenementDaoTest.java
@@ -1,4 +1,5 @@
 package tsi.ensg.jee.services;
 
 public class EvenementDaoTest {
+
 }
--- src/test/java/tsi/ensg/jee/services/ParticipantDaoTest.java
+++ src/test/java/tsi/ensg/jee/services/ParticipantDaoTest.java
@@ -1,4 +1,32 @@
 package tsi.ensg.jee.services;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import tsi.ensg.jee.colloque.metier.Participant;
+import tsi.ensg.jee.colloque.services.ParticipantDao;
+
+import static org.junit.Assert.assertNotNull;
+
 public class ParticipantDaoTest {
+
+    @Test
+    public void testDefaultConstructor() {
+        ParticipantDao participantDao = new ParticipantDao();
+        assertNotNull(participantDao);
+    }
+
+    @Test
+    public void testCreateMethod() {
+        ParticipantDao participantDao = new ParticipantDao();
+
+        Participant participant = new Participant("Amaryllis", "Vignaud", "avignaud@gateau.fr",
+                "25/06/1999", "ENSG", "Fan d'Undertale");
+        participant.setNumPerson(1);
+        long p1 = participantDao.create("Amaryllis", "Vignaud", "avignaud@gateau.fr",
+                "25/06/1999", "ENSG", "Fan d'Undertale");
+
+        Assert.assertEquals(participant, participantDao.get(p1));
+    }
+
 }
