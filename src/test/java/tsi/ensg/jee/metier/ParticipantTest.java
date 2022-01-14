40ca74a51c88d2b0150d84cf6b61c69b2ced3aa9
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-14 14:50:51 +0100

    Complete almost all Participant Tests

--- src/test/java/tsi/ensg/jee/metier/ParticipantTest.java
+++ src/test/java/tsi/ensg/jee/metier/ParticipantTest.java
@@ -17,12 +17,84 @@ public class ParticipantTest {
     @Test
     public void testDefaultConstructeurFullFilled() {
         Participant participant = new Participant("Amaryllis", "Vignaud", "avignaud@gateau.fr",
-                                                "25/06/1999", "ENSG", "Fan d'Undertale");
+                "25/06/1999", "ENSG", "Fan d'Undertale");
+
+        Assert.assertEquals(0, participant.getNumPerson());
         Assert.assertEquals("Amaryllis", participant.getFirstName());
         Assert.assertEquals("Vignaud", participant.getLastName());
         Assert.assertEquals("avignaud@gateau.fr", participant.getEmail());
         Assert.assertEquals("25/06/1999", participant.getBirthDate());
         Assert.assertEquals("ENSG", participant.getOrganisation());
         Assert.assertEquals("Fan d'Undertale", participant.getObservations());
+    }
+
+    @Test
+    public void testDefaultConstructeurSetter() {
+        Participant participant = new Participant();
+
+        participant.setNumPerson(1);
+        participant.setFirstName("Amaryllis");
+        participant.setLastName("Vignaud");
+        participant.setEmail("avignaud@gateau.fr");
+        participant.setBirthDate("25/06/1999");
+        participant.setOrganisation("ENSG");
+        participant.setObservations("Fan d'Undertale");
+
+        Assert.assertEquals(1, participant.getNumPerson());
+        Assert.assertEquals("Amaryllis", participant.getFirstName());
+        Assert.assertEquals("Vignaud", participant.getLastName());
+        Assert.assertEquals("avignaud@gateau.fr", participant.getEmail());
+        Assert.assertEquals("25/06/1999", participant.getBirthDate());
+        Assert.assertEquals("ENSG", participant.getOrganisation());
+        Assert.assertEquals("Fan d'Undertale", participant.getObservations());
+    }
+
+    @Test
+    public void testMethodEqualsTrue(){
+        Participant participant = new Participant("Amaryllis", "Vignaud", "avignaud@gateau.fr",
+                "25/06/1999", "ENSG", "Fan d'Undertale");
+        Participant participant2 = new Participant("Amaryllis", "Vignaud", "avignaud@gateau.fr",
+                "25/06/1999", "ENSG", "Fan d'Undertale");
+
+        Assert.assertTrue(participant.equals(participant2));
+    }
+
+    @Test
+    public void testMethodEqualsFalse(){
+        Participant participant = new Participant("Amaryllis", "Vignaud", "avignaud@gateau.fr",
+                "25/06/1999", "ENSG", "Fan d'Undertale");
+        Participant participant2 = new Participant("ClaireMa", "Alla", "cmalla@gateau.fr",
+                "25/06/1999", "ENSG", "Fan d'Undertale");
+
+        Assert.assertFalse(participant.equals(participant2));
+    }
+
+    @Test
+    public void testHashCodeMethodTrue(){
+        Participant participant = new Participant("Amaryllis", "Vignaud", "avignaud@gateau.fr",
+                "25/06/1999", "ENSG", "Fan d'Undertale");
+        Participant participant2 = new Participant("Amaryllis", "Vignaud", "avignaud@gateau.fr",
+                "25/06/1999", "ENSG", "Fan d'Undertale");
+
+        Assert.assertTrue(participant.hashCode() == participant2.hashCode());
+        Assert.assertTrue(participant.equals(participant2));
+    }
+
+    @Test
+    public void testToStringMethod(){
+        Participant participant = new Participant("Amaryllis", "Vignaud", "avignaud@gateau.fr",
+                "25/06/1999", "ENSG", "Fan d'Undertale");
+
+        String participantToString = "Participant{" +
+                "numPerson=" + participant.getNumPerson() +
+                ", firstName='" + participant.getFirstName() + '\'' +
+                ", lastName='" + participant.getLastName() + '\'' +
+                ", email='" + participant.getEmail() + '\'' +
+                ", birthDate='" + participant.getBirthDate() + '\'' +
+                ", organisation='" + participant.getOrganisation() + '\'' +
+                ", observations='" + participant.getObservations() + '\'' +
+                '}';
+
+        Assert.assertEquals(participantToString, participant.toString());
     }
 }
