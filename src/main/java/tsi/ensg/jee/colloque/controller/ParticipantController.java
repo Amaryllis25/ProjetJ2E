b576850650cb44d23f7541257f101e0b07a6322a
CMAlla <clairemarie.alla@gmail.com>
2022-01-17 10:08:59 +0100

    Changement de database formation en test

--- src/main/java/tsi/ensg/jee/colloque/controller/ParticipantController.java
+++ src/main/java/tsi/ensg/jee/colloque/controller/ParticipantController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import tsi.ensg.jee.colloque.metier.Participant;
 
 @Controller
-public class ApplicationController {
+public class ParticipantController {
 
     @GetMapping("/participants")
     public String getParticipant(Model model) {
-
+        Participant participant = new Participant("CMA", "Alla", "@", "06/12/99", "a", "b");
+        //ParticipantDao participants = new ParticipantDao();
+        //participants.save(part);
+        model.addAttribute("participants",participant); // Ajout au modèle
         return "participant"; //Envoi vers la vue
     }
 }
