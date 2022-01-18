700dd93ae2563bda6677a87b4e95729f27e31c4c
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-18 14:33:06 +0100

    Get participant

--- src/main/java/tsi/ensg/jee/colloque/controller/ParticipantController.java
+++ src/main/java/tsi/ensg/jee/colloque/controller/ParticipantController.java
@@ -15,23 +15,26 @@ public class ParticipantController {
     @Autowired
     ParticipantDao participantDao;
 
-    @GetMapping("/participants")
+
+    @GetMapping("/participant")
     public String create(Model model) {
         Participant participant = new Participant("Boby", "Lapointe",
                 "boby.lapointe@jgv.com", "10/01/1975",
                 "ENSG", "...");
         participantDao.save(participant);
-        model.addAttribute("participants", participant); // Ajout au modèle
+        model.addAttribute("participant", participant); // Ajout au modèle
         return "participant";
     }
 
 
 
-    @GetMapping("/participants/{num_person}")
+    @GetMapping("/participant/{num_person}")
     public String getParticipant(Model model, @PathVariable Long num_person) {
-        Participant participant = participantDao.findById(num_person).get();
-        //participantDao.save(part);
-        model.addAttribute("participants", participant); // Ajout au modèle
+        if (participantDao.findById(num_person).isPresent()){
+            Participant participant = participantDao.findById(num_person).get();
+            //participantDao.save(part);
+            model.addAttribute("participant", participant); // Ajout au modèle
+        }
         return "participant"; //Envoi vers la vue
     }
 }
