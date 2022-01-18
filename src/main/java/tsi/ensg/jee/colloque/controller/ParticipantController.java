9180ac57d7d9cefb31a790b3b29a44feeca5d3aa
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-18 14:47:55 +0100

    Add getbyID method

--- src/main/java/tsi/ensg/jee/colloque/controller/ParticipantController.java
+++ src/main/java/tsi/ensg/jee/colloque/controller/ParticipantController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import tsi.ensg.jee.colloque.metier.Participant;
 import tsi.ensg.jee.colloque.services.ParticipantDao;
 
@@ -12,6 +13,26 @@ public class ParticipantController {
 
     @Autowired
     ParticipantDao participantDao;
+
+    @GetMapping("/participant")
+    public String create(Model model) {
+        Participant participant = new Participant("Boby", "Lapointe",
+                "boby.lapointe@jgv.com", "10/01/1975",
+                "ENSG", "...");
+        participantDao.save(participant);
+        model.addAttribute("participant", participant); // Ajout au modèle
+        return "participant";
+    }
+
+    @GetMapping("/participant/{num_person}")
+    public String getParticipant(Model model, @PathVariable Long num_person) {
+        if (participantDao.findById(num_person).isPresent()){
+            Participant participant = participantDao.findById(num_person).get();
+            //participantDao.save(part);
+            model.addAttribute("participant", participant); // Ajout au modèle
+        }
+        return "participant"; //Envoi vers la vue
+    }
 
     @GetMapping("/allParticipants")
     public String getParticipants(Model model) {
