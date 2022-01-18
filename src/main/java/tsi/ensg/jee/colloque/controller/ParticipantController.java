d6213c80f205835e211a7834c0f4063668b70cf4
CMAlla <clairemarie.alla@gmail.com>
2022-01-18 17:39:41 +0100

    Ajout formulaire participant

--- src/main/java/tsi/ensg/jee/colloque/controller/ParticipantController.java
+++ src/main/java/tsi/ensg/jee/colloque/controller/ParticipantController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import tsi.ensg.jee.colloque.metier.Participant;
 import tsi.ensg.jee.colloque.services.ParticipantDao;
 
@@ -13,19 +15,29 @@ public class ParticipantController {
     @Autowired
     ParticipantDao participantDao;
 
-    @GetMapping("/allParticipants")
+    @GetMapping("/participants")
     public String getParticipants(Model model) {
-        Participant part= new Participant("CMA", "Alla", "@", "06/12/99", "a", "b");
-        participantDao.save(part);
-
-        Participant part2= new Participant("CMA", "Alla", "@", "06/12/99", "a", "b");
-        participantDao.save(part2);
-        model.addAttribute("allParticipants",participantDao.findAll()); // Ajout au modèle
+        model.addAttribute("participants",participantDao.findAll()); // Ajout au modèle
         return "participants"; //Envoi vers la vue
     }
 
-    @GetMapping("/createParticipant")
+    @GetMapping("/createPartForm")
     public String createParticipant(Model model) {
-        return "createParticipant"; //Envoi vers la vue
+        //https://www.baeldung.com/thymeleaf-list
+        ParticipantDao partForm = new ParticipantDao();
+
+        //Participant part= new Participant("CMA", "Alla", "@", "06/12/99", "a", "b");
+
+        partForm.save(new Participant());
+
+        model.addAttribute("form", partForm);
+        return "createPartForm"; //Envoi vers la vue
+    }
+
+    @PostMapping("/participants")
+    public String saveParticipant(@ModelAttribute ParticipantDao form, Model model) {
+        //participantDao.save(form.findAll());
+        model.addAttribute("participants", participantDao.findAll());
+        return "redirect:/participants";
     }
 }
