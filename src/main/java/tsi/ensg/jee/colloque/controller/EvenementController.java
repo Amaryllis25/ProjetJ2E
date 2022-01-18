04edc541bdc0381b3ada9d66f4e3d77550099bb8
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-18 14:57:16 +0100

    GetById event method

--- src/main/java/tsi/ensg/jee/colloque/controller/EvenementController.java
+++ src/main/java/tsi/ensg/jee/colloque/controller/EvenementController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import tsi.ensg.jee.colloque.metier.Evenement;
+import tsi.ensg.jee.colloque.metier.Participant;
 import tsi.ensg.jee.colloque.services.EvenementDao;
 
 @Controller
@@ -12,8 +15,22 @@ public class EvenementController {
     @Autowired
     EvenementDao evenementDao;
 
-    @GetMapping("/evenements")
-    public String getEvenement(Model model) {
+    @GetMapping("/evenement")
+    public String create(Model model) {
+        Evenement evenement = new Evenement("Undertale party", "Undertale", "22/04/2022", 3,
+                100000, "Events for Undertale's fans", "Toby Fox", "Trailer");
+        evenementDao.save(evenement);
+        model.addAttribute("evenement", evenement); // Ajout au modèle
+        return "evenement";
+    }
+
+    @GetMapping("/participant/{num_even}")
+    public String getEvenement(Model model, @PathVariable Long num_even) {
+        if (evenementDao.findById(num_even).isPresent()){
+            Evenement evenement = evenementDao.findById(num_even).get();
+            //participantDao.save(part);
+            model.addAttribute("evenement", evenement); // Ajout au modèle
+        }
         return "evenement"; //Envoi vers la vue
     }
 }
