77b9b4b3b860d87f3c90ea0542c8ca87a970bdd6
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-19 12:13:28 +0100

    Add EvenementDao comments

--- src/main/java/tsi/ensg/jee/colloque/services/EvenementDao.java
+++ src/main/java/tsi/ensg/jee/colloque/services/EvenementDao.java
@@ -14,19 +14,37 @@ public class EvenementDao {
     @Autowired
     private EvenementRepo repository;
 
+    /**
+     * Function which find all events in the repository
+     * @return evenements
+     */
     public List<Evenement> findAll() {
         List<Evenement> evenements = (List<Evenement>) repository.findAll();
         return evenements;
     }
 
+    /**
+     * Function which save a event in the repository
+     * @param evenement data of an event
+     */
     public void save(Evenement evenement) {  // pas testé avec persist !
         repository.save(evenement);
     }
 
+    /**
+     * Function which search an event by id and return it if exists
+     * @param id id of the event
+     * @return repository.findById(id)
+     */
     public Optional<Evenement> findById(Long id) {
         return repository.findById(id);
     }
 
+    /**
+     * Function which delete an event
+     * @param evenement data of an event
+     * @return repository.findById(id)
+     */
     public void delete(Evenement evenement) {
         repository.delete(evenement);
     }
