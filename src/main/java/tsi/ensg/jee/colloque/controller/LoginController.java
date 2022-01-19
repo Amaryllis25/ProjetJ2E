2ebab707c297b3c9353e5161ca3230c2d991437d
CMAlla <clairemarie.alla@gmail.com>
2022-01-19 18:56:18 +0100

    Tentative redirection apres login

--- src/main/java/tsi/ensg/jee/colloque/controller/LoginController.java
+++ src/main/java/tsi/ensg/jee/colloque/controller/LoginController.java
@@ -6,9 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class LoginController implements WebMvcConfigurer {
-
+    
 	public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+		registry.addViewController("/site").setViewName("site");
         registry.addViewController("/login").setViewName("login");
-		registry.addViewController("/index").setViewName("index");
 	}
 }
