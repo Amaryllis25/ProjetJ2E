06cbf60b9f9992d29e590817851c2eee4558cb71
CMAlla <clairemarie.alla@gmail.com>
2022-01-25 17:44:43 +0100

    Debut de recherche de participant

--- src/main/java/tsi/ensg/jee/colloque/services/ParticipantDao.java
+++ src/main/java/tsi/ensg/jee/colloque/services/ParticipantDao.java
@@ -23,6 +23,21 @@ public class ParticipantDao {
         repository.save(participant);
     }
 
+/*
+    //Get all employees whose Name or Last Name match the searchParam, order by name and last name.
+    public List<Participant> searchEmployeesByName(String searchParam) {
+        Comparator<Participant> groupByComparator = Comparator.comparing(Participant::getFirstName)
+                                                    .thenComparing(Participant::getLastName);
+        List<Participant> result = this.findAll()
+                .stream()
+                .filter(e -> e.getFirstName().equalsIgnoreCase(searchParam) || e.getLastName().equalsIgnoreCase(searchParam))
+                .sorted(groupByComparator)
+                .collect(Collectors.toList());
+        return result;
+    }
+// https://www.oracle.com/webfolder/technetwork/tutorials/obe/java/basic_app_embedded_tomcat/basic_app-tomcat-embedded.html
+*/
+
     public Optional<Participant> findById(Long id) {
         return repository.findById(id);
     }
