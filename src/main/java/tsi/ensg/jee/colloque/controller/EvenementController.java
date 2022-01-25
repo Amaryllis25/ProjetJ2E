e227a8cf471cc9b5546b89bc5ac8e6502b44cb47
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-25 13:21:02 +0100

    Add add function in Controller not in html

--- src/main/java/tsi/ensg/jee/colloque/controller/EvenementController.java
+++ src/main/java/tsi/ensg/jee/colloque/controller/EvenementController.java
@@ -5,13 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import tsi.ensg.jee.colloque.metier.Evenement;
+import tsi.ensg.jee.colloque.metier.Participant;
 import tsi.ensg.jee.colloque.services.EvenementDao;
+import tsi.ensg.jee.colloque.services.ParticipantDao;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class EvenementController {
 
     @Autowired
     EvenementDao evenementDao;
+
+    @Autowired
+    ParticipantDao participantDao;
 
     /**
      * Function which get all events
@@ -21,6 +29,7 @@ public class EvenementController {
     @GetMapping("/evenements")
     public String getEvenements(Model model) {
         model.addAttribute("evenements", evenementDao.findAll()); // Ajout au modèle
+        model.addAttribute("participants", participantDao.findAll());
         return "evenements"; //Envoi vers la vue
     }
 
@@ -35,6 +44,7 @@ public class EvenementController {
         if (evenementDao.findById(numEvent).isPresent()){
             Evenement evenement = evenementDao.findById(numEvent).get();
             model.addAttribute("evenement", evenement); // Ajout au modèle
+            model.addAttribute("participants", participantDao.findAll());
         }
         return "evenement"; //Envoi vers la vue
     }
@@ -63,8 +73,13 @@ public class EvenementController {
         return "redirect:/evenements";
     }
 
+    /**
+     * Function which update an event to evenement.html
+     * @param model the view
+     * @return View evenement.html
+     */
     @GetMapping("/editEvenement/{numEvent}")
-    public String editParticipant(Model model, @PathVariable("numEvent") Long numEvent) {
+    public String editEvenement(Model model, @PathVariable("numEvent") Long numEvent) {
         Evenement evenement = evenementDao.findById(numEvent).get();
         model.addAttribute("evenement", evenement);
         return "createEvenForm";
@@ -79,16 +94,24 @@ public class EvenementController {
     @GetMapping("/deleteEvent/{numEvent}")
     public String deleteEvenement(Model model, @PathVariable("numEvent") Long numEvent) {
         Evenement evenement = evenementDao.findById(numEvent).get();
-        System.out.println("test");
         evenementDao.delete(evenement);
         return "redirect:/evenements";
-    } //Envoi vers la vue
+    } //Envoi vers la vu
 
-    /**
-     * Function which update an event to evenement.html
-     * @param model the view
-     * @return View evenement.html
-     */
-    @PutMapping("/evenementUpdate/{numEvent}")
-    public String updateEvenement(Model model, @PathVariable Long numEvent) {return "evenement";} //Envoi vers la vue
+
+    @GetMapping("/evenement/{numEvent}/add/{numPerson}")
+    public String addParticipant(Model model, @PathVariable("numEvent") Long numEvent, @PathVariable("numPerson") Long numPerson) {
+
+        Evenement evenement = evenementDao.findById(numEvent).get();
+        System.out.println(participantDao.findById(numPerson).get());
+
+        List<Participant> participants = new ArrayList<>();
+        evenement.setParticipants(participants);
+        evenement.addParticipant(participantDao.findById(numPerson).get());
+
+        System.out.println();
+        model.addAttribute("evenement", evenement);
+        System.out.println(evenement);
+        return "redirect:/evenement/" + numEvent;
+    }
 }
